463f1d1b29536f351941d35085ba2af3b268a115
mausayao <mauricio.sayao@gmail.com>
2019-04-15 21:28:08 -0400

    Initial Commit

---
+++ eggplant-brownie/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  eggplant-brownie
+//
+//  Created by Maurício de Freitas Sayão on 15/04/19.
+//  Copyright © 2019 Maurício de Freitas Sayão. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ eggplant-brownieTests/eggplant_brownieTests.swift
@@ -0,0 +1,34 @@
+//
+//  eggplant_brownieTests.swift
+//  eggplant-brownieTests
+//
+//  Created by Maurício de Freitas Sayão on 15/04/19.
+//  Copyright © 2019 Maurício de Freitas Sayão. All rights reserved.
+//
+
+import XCTest
+@testable import eggplant_brownie
+
+class eggplant_brownieTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
