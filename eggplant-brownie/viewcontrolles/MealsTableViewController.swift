7b1def4d89436f80414d1d5e91fc20c99b08895d
mausayao <mauricio.sayao@gmail.com>
2019-04-20 17:02:26 -0400

    tabela dinamica

---
+++ eggplant-brownie/viewcontrolles/MealsTableViewController.swift
@@ -0,0 +1,26 @@
+//
+//  MealsTableViewController.swift
+//  eggplant-brownie
+//
+//  Created by Maurício de Freitas Sayão on 20/04/19.
+//  Copyright © 2019 Maurício de Freitas Sayão. All rights reserved.
+//
+
+import UIKit
+
+class MealsTableViewController: UITableViewController {
+    let meals = [Meal(names: "Feijão com arroz", happiness: 5),
+    Meal(names: "Bolo de cenoura", happiness: 3)]
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return meals.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let row = indexPath.row
+        let meal = meals[row]
+        let cell = UITableViewCell(style: UITableViewCell.CellStyle.default, reuseIdentifier: nil)
+        cell.textLabel!.text = meal.names
+        return cell
+    }
+}
